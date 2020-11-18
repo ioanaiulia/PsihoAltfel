8c13a8e85cec4544e4d525cc7b99fc343634edab
ioanaiulia <iuliamuresan2013@gmail.com>
2020-11-18 23:04:23 +0200

    prepared prod env

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/website'));
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/website/index.html'));
-});
+const path = require('path');
+
+app.use(express.static(__dirname + '/PsihoAltfel'));
+
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/PsihoAltfel/index.html'));
+});
+
+ console.log('Console listening!');
