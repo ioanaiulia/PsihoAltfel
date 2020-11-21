bd6a4528d8f83a14b4799fb26fdef4c7c3f20bd1
ioanaiulia <iuliamuresan2013@gmail.com>
2020-11-21 18:56:42 +0200

    done with memory

--- src/app/body/superior-process/memory/memory.component.ts
+++ src/app/body/superior-process/memory/memory.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-memory',
   templateUrl: './memory.component.html',
-  styleUrls: ['./memory.component.css']
+  styleUrls: ['./memory.component.css', '../../sensitive-process/senzitivity/senzitivity.component.css']
 })
 export class MemoryComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  whatIsMm = false;
+
+  onWhatIsMmClick() {
+    this.whatIsMm = !this.whatIsMm;
+  } 
+
+  mmCaracteristic = false;
+
+  onMmCaracteristicClick() {
+    this.mmCaracteristic = !this.mmCaracteristic;
+  }
+
+  mmPandF = false;
+
+  onMmPandFClick() {
+    this.mmPandF = !this.mmPandF;
+  }
+
+  mmFactors = false;
+
+  onMmFactorsClick() {
+    this.mmFactors = !this.mmFactors;
+  }
+
+  forgetting = false;
+
+  onForgettingClick() {
+    this.forgetting = !this.forgetting;
   }
 
 }
