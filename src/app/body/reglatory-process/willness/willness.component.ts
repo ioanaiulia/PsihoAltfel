fa620ad3d06eb7a26d1595b3d66a445b902b5915
ioanaiulia <iuliamuresan2013@gmail.com>
2020-11-21 23:27:29 +0200

    done with willness

--- src/app/body/reglatory-process/willness/willness.component.ts
+++ src/app/body/reglatory-process/willness/willness.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-willness',
   templateUrl: './willness.component.html',
-  styleUrls: ['./willness.component.css']
+  styleUrls: ['./willness.component.css', '../../sensitive-process/senzitivity/senzitivity.component.css']
 })
 export class WillnessComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  whatIsV = false;
+
+  onWhatIsVClick() {
+    this.whatIsV = !this.whatIsV;
+  }
+
+  cv = false;
+
+  onCVClick() {
+    this.cv = !this.cv;
+  }
+
+  vsteps = false;
+
+  onVStepsClick() {
+    this.vsteps = !this.vsteps;
   }
 
 }
