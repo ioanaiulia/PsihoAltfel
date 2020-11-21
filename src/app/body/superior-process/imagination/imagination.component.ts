17dbf93dc1b181de287aa93c4a3baffc7169e0a0
ioanaiulia <iuliamuresan2013@gmail.com>
2020-11-21 19:48:20 +0200

    imagination done

--- src/app/body/superior-process/imagination/imagination.component.ts
+++ src/app/body/superior-process/imagination/imagination.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-imagination',
   templateUrl: './imagination.component.html',
-  styleUrls: ['./imagination.component.css']
+  styleUrls: ['./imagination.component.css', '../../sensitive-process/senzitivity/senzitivity.component.css']
 })
 export class ImaginationComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  whatIsImg = false;
+
+  onWhatIsImgClick() {
+    this.whatIsImg = !this.whatIsImg;
+  }
+
+  imgForms = false;
+
+  onImgFormsClick() {
+    this.imgForms = !this.imgForms;
+  }
+
+  imgProc = false;
+
+  onImgProcClick() {
+    this.imgProc = !this.imgProc;
+  }
+
+  imgCaracteristic = false;
+
+  onImgCaracteristicClick() {
+    this.imgCaracteristic = !this.imgCaracteristic;
   }
 
 }
