e6337d2192e9bbd2dd3d0423ccaaeb1911716386
ioanaiulia <iuliamuresan2013@gmail.com>
2020-11-22 20:43:31 +0200

    done with psychic-activity

--- src/app/body/psychic-activities/psychic-activities.component.ts
+++ src/app/body/psychic-activities/psychic-activities.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-psychic-activities',
   templateUrl: './psychic-activities.component.html',
-  styleUrls: ['./psychic-activities.component.css']
+  styleUrls: ['./psychic-activities.component.css', '../sensitive-process/senzitivity/senzitivity.component.css']
 })
 export class PsychicActivitiesComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  whatIsAct = false;
+
+  onWhatIsActClick() {
+    this.whatIsAct = !this.whatIsAct;
+  }
+
+  limb = false;
+
+  onLimbClick() {
+    this.limb = !this.limb;
   }
 
 }
