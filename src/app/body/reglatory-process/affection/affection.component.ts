b6a79c467fac83cfd8638ecd018236045f6d2ee7
ioanaiulia <iuliamuresan2013@gmail.com>
2020-11-22 19:33:31 +0200

    done with affection

--- src/app/body/reglatory-process/affection/affection.component.ts
+++ src/app/body/reglatory-process/affection/affection.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-affection',
   templateUrl: './affection.component.html',
-  styleUrls: ['./affection.component.css']
+  styleUrls: ['./affection.component.css', '../../sensitive-process/senzitivity/senzitivity.component.css']
 })
 export class AffectionComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  whatIsAF = false;
+
+  onWhatIsAFClick() {
+    this.whatIsAF = !this.whatIsAF;
+  }
+
+  caract = false;
+  
+  onCaracteristciClick() {
+    this.caract = !this.caract;
+  }
+
+  clas = false;
+  onClasificareClick() {
+    this.clas = !this.clas;
   }
 
 }
